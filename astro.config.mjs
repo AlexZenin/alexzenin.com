6e5993b640c9741d34542261f584914b9d69abe0
Alex Zenin <alex@alexzenin.com>
2023-03-02 19:57:08 +1100

    build: add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import preact from '@astrojs/preact'
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
-})
+    integrations: [preact(), tailwind(), mdx({
+        syntaxHighlight: 'shiki',
+    })]
+});
